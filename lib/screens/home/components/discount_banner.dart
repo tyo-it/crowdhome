b5f83dbae5e6c60fcf5178cd2b8e16cf7e253733
Istantyo <istantyo@xfers.com>
2021-04-13 21:43:35 +0700

    change discount color

--- lib/screens/home/components/discount_banner.dart
+++ lib/screens/home/components/discount_banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 
 import '../../../size_config.dart';
 
@@ -18,7 +19,7 @@ class DiscountBanner extends StatelessWidget {
         vertical: getProportionateScreenWidth(15),
       ),
       decoration: BoxDecoration(
-        color: Color(0xFF4A3298),
+        color: kPrimaryColor,
         borderRadius: BorderRadius.circular(20),
       ),
       child: Text.rich(
